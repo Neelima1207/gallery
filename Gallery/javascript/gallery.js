6accf5000f8ad7a436478886b1ad29ca4edd6b0b
Neelima1207 <148609144+Neelima1207@users.noreply.github.com>
2023-10-21 11:18:24 +0530

    Add files via upload

---
+++ Gallery/javascript/gallery.js
@@ -0,0 +1,62 @@
+// Gallery Images Code
+var allImg=document.images;
+function allImgTags(){
+    for(i=1;i<=allImg.length;i++){
+        allImg[i].src=`./asset/images/${i}.jpg`
+        allImg[i].style.width="200px"
+        allImg[i].style.height="180px"
+        allImg[i].addEventListener("click",function(){
+            document.getElementById("bigImage").style.display="flex"
+        })
+    }
+}
+allImgTags()
+
+var globalSrc;
+
+function passSrc(urlOfimg){
+
+    document.getElementById("bigimg").style.backgroundImage=`url(${urlOfimg})`
+    globalSrc=urlOfimg;
+}
+
+//cosewindow
+
+function closewindow(){
+    document.getElementById("bigImage").style.display="none"
+    document.getElementById("galleryImageEdit").style.display="none"
+}
+function openEditWindow(){
+    document.getElementById("galleryImageEdit").style.display="flex"
+    document.getElementById("image").style.backgroundImage=`url(${globalSrc})`
+}
+
+// image edit code
+
+function imageEdit(){
+    var blurValue=document.getElementById("blur").value
+    var brightnessValue=document.getElementById("brightness").value
+    var contrastValue=document.getElementById("contrast").value
+    var grayscaleValue=document.getElementById("grayscale").value
+    var hueValue=document.getElementById("hue").value
+    var invertValue=document.getElementById("invert").value
+    var opacityValue=document.getElementById("opacity").value
+    var saturateValue=document.getElementById("saturate").value
+    var sepiaValue=document.getElementById("sepia").value
+}
+
+document.getElementById("image").style.filter=`blur(${blurValue}px) brightness(${brightnessValue}%)  contrast(${contrastValue}%) grayscale(${grayscaleValue}%) hue-rotate(${hueValue}deg) invert(${invertValue}%) opacity(${opacityValue}%) saturate(${saturateValue}%) sepia(${sepiaValue}%)`
+
+// for transform code
+
+var rotateX=document.getElementById("rotatex").value
+var rotateY=document.getElementById("rotatey").value
+var rotateZ=document.getElementById("rotatez").value
+var scaleX=document.getElementById("scalex").value
+var scaleY=document.getElementById("scaley").value
+var scaleZ=document.getElementById("scalez").value
+var skewX=document.getElementById("skewx").value
+var skewY=document.getElementById("skewy").value
+var translateX=document.getElementById("translatex").value
+var translateY=document.getElementById("translatey").value
+document.getElementById("image").style.transform=`rotate(${rotateZ}deg) rotateX(${rotatex}deg)  rotateY(${rotatey}deg) scaleX(${scalex}) scaleY(${scaley}) skewX(${skewX}deg) skewY(${skewy}deg) translateX(${translatex}px) translateY(${translatey}px)`
